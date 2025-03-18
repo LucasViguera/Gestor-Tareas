2f9e54f04450d543ec5b71fcf6a58b8cf9c00407
LucasViguera <lucas.viguera2001@gmail.com>
2025-03-18 10:12:32 -0300

    mejoras v1.1

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './components/home/home.component';
 import { TaskCreateComponent } from './components/task-create/tasks-create.component';
 import { TaskComponent } from './components/tasks-list/tasks-list.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'task-create', component: TaskCreateComponent },
   { path: 'task', component: TaskComponent },
   { path: 'calendar', component: CalendarComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module'; // Importamos el módulo de rutas
-
+import { FormsModule } from '@angular/forms';
+import { TaskComponent } from './components/tasks-list/tasks-list.component';  // Importa el componente de listado de tareas
+import { TaskService } from './task.service';  // Importa el servicio de tareas
 import { TaskCreateComponent } from './components/task-create/tasks-create.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [], // No necesitamos declarar componentes standalone aquí
+  declarations: [
+  ], // No necesitamos declarar componentes standalone aquí
   imports: [
     BrowserModule,
     AppRoutingModule,  // Usamos AppRoutingModule para las rutas
     RouterModule,      // RouterModule también para manejar las rutas en general
-    TaskCreateComponent,
     HomeComponent,
+    FormsModule,
+    TaskCreateComponent,
+    TaskComponent,
   ],
+  providers: [TaskService],
 })
 export class AppModule {}

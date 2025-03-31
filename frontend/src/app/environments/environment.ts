ff279c9b8fecc7e02e0d389ceb47674809b934a9
LucasViguera <lucas.viguera2001@gmail.com>
2025-03-31 17:39:23 -0300

    subir variable de entorno del backend

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://gestortareas.com',  // URL de la API en producción
+    apiUrl: 'https://gestor-tareas-production.up.railway.app/',  // URL de la API en producción
   };
   
